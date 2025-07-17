67de3857e0a8d316b57497aebe1106e8b2382151
ahsaniayu1 <amalia.ahsani@bsi.co.id>
2025-07-17 14:41:34 +0700

    Add files via upload

---
+++ View Exercise/SQLQuery Case 6.sql
@@ -0,0 +1,17 @@
+create view vw_SalespersonPerformance
+as
+
+select
+    p.FirstName + ' ' + isnull(p.MiddleName + ' ', '') + p.LastName as FullName,
+    count(o.SalesOrderID) as OrderCount,
+    sum(o.TotalDue) as TotalSales
+from
+    Sales.SalesOrderHeader o
+    join Sales.SalesPerson sp on o.SalesPersonID = sp.BusinessEntityID
+    join Person.Person p on sp.BusinessEntityID = p.BusinessEntityID
+where
+    o.SalesPersonID is not null
+group by
+    p.FirstName,
+    p.MiddleName,
+    p.LastName;
